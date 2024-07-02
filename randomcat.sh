62541e96fec6cad2d4065828d2e51487b295502a
junaidali-b <junaidali.barodawala110@gmail.com>
2024-07-02 17:49:31 +0100

    Changed the cat ascii art generator to hide jumping cursor for gifs.

--- randomcat.sh
+++ randomcat.sh
@@ -1,3 +1,10 @@
+#Hiding cursor
+printf "\e[?25l"
+
 clear
+
 echo "Here's a fat kitty for you!"
 curl -s https://api.thecatapi.com/v1/images/search | jq -r '.[0].url' | xargs ascii-image-converter -bC --dither
+
+# Show cursor again
+printf "\e[?25h"
