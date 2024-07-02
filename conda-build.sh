326fcce91cd6024fd7f170677ffd267755fb2282
junaidali-b <junaidali.barodawala110@gmail.com>
2024-07-02 17:46:31 +0100

    Added a script for removing Plasma bulkware and another script for creating a conda environment and installing packages in Linux.

---
+++ conda-build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#Installing conda
+cd /tmp
+curl -O https://repo.anaconda.com/archive/Anaconda3-2024.06-1-Linux-x86_64.sh
+bash Anaconda3-2024.06-1-Linux-x86_64.sh
+eval "$(/home/junaid/anaconda3/bin/conda shell.bash hook)"
+source ~/.bashrc
+conda init
+conda config --set auto_activate_base true
+
+#Creating conda environment
+conda create --name ml python=3.10
+conda activate ml
+
+#Installing conda packages
+
+#Part 1
+conda install -c conda-forge airflow apache-airflow arrow beautifulsoup4 bottleneck cudatoolkit dbt docker-py email-validator google-auth jedi
+
+#Part 2
+conda install -c conda-forge jupyter_core jupyterlab keras matplotlib mysql-libs mysql-common numpy openshift-cli openssl pandas pip plotnine
+
+#Part 3
+conda install pytorch:pytorch
+
+#Part 4
+conda install -c conda-forge r-base scikit-learn scipy seaborn sqlite tensorflow wget yaml
+
+#Exiting environment
+conda deactivate
+
+exit
---
+++ remove-plasma-bulk.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+clear
+
+echo "Say goodbye to all the bloatware Plasma installs for you :)"
+
+#Head to https://archlinux.org/groups/x86_64/kde-applications/ for a full list of applications, to add or remove packages to be uninstalled in the following list.
+
+sudo pacman -Rns accessibility-inspector akonadi-calendar-tools akonadi-import-wizard akonadiconsole akregator alligator angelfish arianna artikulate audex audiocd-kio blinken bomber bovo cantor cervisia colord-kde elisa filelight francis ghostwriter granatier grantlee-editor isoimagewriter itinerary juk k3b kaddressbook kajongg kalgebra kalm kalzium kamera kanagram kapman kapptemplate kasts katomic kblackbox kblocks kbounce kbreakout kbruch kcharselect kclock kcron kde-dev-scripts kde-dev-utils kde-inotify-survey kdebugsettings kdenlive kdepim-addons kdesdk-kio kdesdk-thumbnailers kdevelop kdevelop-php kdevelop-python kdialog kdiamond keditbookmarks keysmith kfourinline kgeography kget kgoldrunner kgpg khangman kig kigo killbots kimagemapeditor kio-admin kio-gdrive kio-zeroconf kirigami-gallery kiriki kiten kjumpingcube kleopatra klettres klickety klines kmahjongg kmail kmail-account-wizard kmines kmix kmousetool kmouth kmplot knavalbattle knetwalk knights knotes kolf kollision kongress konquest kontact konversation korganizer kpat krdc krecorder kreversi krfb kruler kshisen ksirk ksnakeduel kspaceduel ksquares ksudoku kteatime ktrip ktuberling kturtle kubrick kwave kweather kwordquiz lokalize lskat marble markdownpart mbox-importer merkuro minuet neochat palapeli parley picmi pim-data-exporter pim-sieve-editor poxml skanlite skanpage skladnik step svgpart sweeper telly-skout tokodon umbrello yakuake zanshin
+
+clear
+
+echo "The junk has left the chat"
